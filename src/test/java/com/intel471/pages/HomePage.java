699e0052034081d8d176f32192e9df1402bbfeaa
sukruaydin <sukraydin29@gmail.com>
2023-01-11 00:17:50 +0300

    5th Commit

--- src/test/java/com/intel471/pages/HomePage.java
+++ src/test/java/com/intel471/pages/HomePage.java
@@ -5,13 +5,9 @@ import com.intel471.utils.Driver;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.ArrayList;
 import java.util.List;
 
