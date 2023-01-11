ef622c9ef3f4d0ee06b246bec2f2b6e07913dba2
sukruaydin <sukraydin29@gmail.com>
2023-01-11 17:58:43 +0300

    8th Commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Case Study Assessment by Intel471
 
-In this repository, 2 test automation case studies (UI&API) assigned by Intel471 are performed by following Cucumber BBD Framework.  
+In this repository, 2 test automation case studies (UI & API) assigned by Intel471 are performed by following Cucumber BBD Framework.  
 
 ## Instructions
 Test Scenarios are prepared by;
@@ -12,8 +12,6 @@ QA Automation Engineer
 sukraydin29@gmail.com
 
 https://www.linkedin.com/in/şükrü-aydın
-
----
 
 ### Build Tool
 ```Maven```
@@ -34,5 +32,11 @@ https://www.linkedin.com/in/şükrü-aydın
 ### System Requirements
 ```Java 8 + SDK```
 
-### Additional Info
-I automated UI case on amazon.com
+### Nice to Know
+Having 2 layers;
+    1. Business Layer --> src > test > resources > features
+    1. Implementation Layer --> src > java > com > intel471 > step_defs
+Automated UI case on amazon.com by using recently created credentials.
+Followed Page Object Model and Singleton Design Pattern.
+
+
--- src/test/java/com/intel471/step_defs/StepDefs_API.java
+++ src/test/java/com/intel471/step_defs/StepDefs_API.java
@@ -14,27 +14,32 @@ public class StepDefs_API {
 
     @Given("User sends GET request to retrieve all employees")
     public void userSendsGETRequestToRetrieveAllEmployees() {
+        //gets all employees as Response object, sets it as class level
         allEmployees = APIUtils.getAllEmployees();
         //allEmployees.prettyPrint();
     }
 
     @When("User finds the number of employees older than {int} and their ID values")
     public void userFindsTheNumberOfEmployeesOlderThanAndTheirIDValues(int age) {
+        //prints number&ids of employees older than given age
         APIUtils.numberOfEmployeesOlderThanAge(allEmployees,age);
     }
 
     @Given("User sends POST request to create new employee older than {int}")
     public void userSendsPOSTRequestToCreateNewEmployeeOlderThan(int age) {
+        //creates a new employee, sets its id number as class level
         id = APIUtils.createAnEmployeeOlderThanAge(age);
     }
 
     @Given("User updates the recently created employee {string}, {int}, {int}")
     public void userUpdatesTheRecentlyCreatedEmployee(String name, int salary, int age) {
+        //updates recently created employee's data
         APIUtils.updateAnEmployee(id,name,salary,age);
     }
 
     @Then("User deletes recently created employee \\(id number : {int} )")
     public void userDeletesRecentlyCreatedEmployeeIdNumber(int id) {
+        //removes employee by given id
         APIUtils.deleteAnEmployee(id);
     }
 }
